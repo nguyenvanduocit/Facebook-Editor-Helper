7a386e7625e53e231e969b96cf007681b22845c9
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-10-28 13:53:05 +0700

    init

---
+++ js/ZipCrawler.js
@@ -0,0 +1,90 @@
+(function(){
+
+    var ZipCrawler = function(){
+        "use strict";
+        this.zipCode = [];
+    };
+    ZipCrawler.prototype.loadData = function(){
+        "use strict";
+        var self = this;
+        $.getJSON( "js/data.json", function( data ) {
+            self.zipCode = data;
+            console.log(self.zipCode);
+        });
+    };
+    ZipCrawler.prototype.saveData = function(){
+        "use strict";
+        chrome.storage.sync.set({"zipCode":this.zipCode}, function(){
+            console.log('Saved');
+        });
+    };
+    ZipCrawler.prototype.onRecivePlace = function(data){
+        "use strict";
+        return this.addNode(this.zipCode, data);
+    };
+    ZipCrawler.prototype.onReciveRegion = function(data){
+        "use strict";
+        return this.addNode(this.zipCode, data);
+    };
+    ZipCrawler.prototype.addNode = function(collection, data){
+        "use strict";
+        //console.log('before add : ', collection, data);
+        var self = this;
+        if(Array.isArray(collection)){
+            if(collection.length > 0) {
+                for (var index = 0; index < collection.length; index++) {
+                    let node = collection[index];
+                    var result = self.addNode(node, data);
+                    if (result) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else{
+                collection.push(data);
+                return true;
+            }
+        }
+        else{
+            if((typeof collection.slug !='undefined') && (collection.slug == data.slug)){
+                collection.list = data.list;
+                return true;
+            }else{
+                if((typeof collection.list != "undefined") && Array.isArray(collection.list)){
+                    return self.addNode(collection.list, data);
+                }
+                else{
+                    return false
+                }
+            }
+        }
+    };
+    ZipCrawler.prototype.initEventHandler = function(){
+        "use strict";
+        var self = this;
+        chrome.runtime.onMessage.addListener(
+            function(request, sender, sendResponse) {
+                var result = null;
+                switch (request.event){
+                    case "post_places":
+                        result = self.onRecivePlace(request);
+                        break;
+                    case "post_regions":
+                        result = self.onReciveRegion(request);
+                        break;
+                }
+                if(result != null){
+                    //self.saveData();
+                    sendResponse({success:result, data:self.zipCode});
+                }
+            });
+    };
+    /**
+     * Create new instance of application
+     * @type {ZipCrawler}
+     */
+    var crawler = new ZipCrawler();
+    crawler.loadData();
+    crawler.initEventHandler();
+})();
--- js/zipCrawler.content.js
+++ js/zipCrawler.content.js
@@ -1,11 +1,6 @@
 (function ($) {
     getRegion();
     getPlace();
-    var clipboard = new Clipboard('#btn-copy', {
-        text: function (trigger) {
-            return $("#parsed_data").text();
-        }
-    });
     function getPlace(){
         var $placeList = $('tr[itemtype="http://schema.org/City"]');
         if($placeList.length > 0){
@@ -18,23 +13,25 @@
 
                     var $target = $(this);
                     var $tds = $target.find('td');
-                    console.log($tds);
                     var $name = $tds.eq(0);
                     var $suburb = $name.find('span');
                     if($suburb.length >0){
-                        place.count = $suburb.text();
+                        place.suburb = $suburb.text();
                     }
                     else{
-                        place.count = '';
+                        place.suburb = '';
                     }
-                    place.name = $name.text().replace(place.count,'');
+                    place.name = $name.text().replace(place.suburb,'');
                     place.zipCode = $tds.eq(2).text();
-
+                    place.id = $tds.eq(3).find('.rec').attr('id');
                     placeList.push(place);
                 });
-                $('#browse').prepend('<p id="parsed_data">'+JSON.stringify(placeList)+'</p>');
+
+                chrome.runtime.sendMessage({event: "post_places", list:placeList, slug:window.location.pathname.replace('/','')}, function(response) {
+                    $('#browse').append('<p id="parsed_data">'+JSON.stringify(response.data)+'</p>');
+                });
             });
-            $('#browse').prepend(grabButton);
+            $('#browse').append(grabButton);
         }
     }
     function getRegion(){
@@ -43,7 +40,7 @@
             var grabButton = $('<button id="btn-copy">Get Region</button>');
             grabButton.on('click', function(){
                 $placeList = $('tr[itemtype="http://schema.org/AdministrativeArea"]');
-                var placeList = [];
+                var regionList = [];
                 $placeList.each(function () {
                     var place = {};
 
@@ -61,13 +58,14 @@
                         place[codeType] = $target.text();
                     });
 
-                    place.totalPlace = $tds.eq(2).text();
-                    place.child = [];
-                    placeList.push(place);
+                    place.totalPlace = parseInt($tds.eq(2).text().replace(' ', '').trim());
+                    regionList.push(place);
                 });
-                $('#browse').prepend('<p id="parsed_data">'+JSON.stringify(placeList)+'</p>');
+                chrome.runtime.sendMessage({event: "post_regions", list:regionList, slug:window.location.pathname.replace('/','')}, function(response) {
+                    $('#browse').append('<p id="parsed_data">'+JSON.stringify(response.data)+'</p>');
+                });
             });
-            $('#browse').prepend(grabButton);
+            $('#browse').append(grabButton);
         }
     }
 })(jQuery);
